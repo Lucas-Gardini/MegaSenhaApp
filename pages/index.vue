721de8ecb41b95855b63b02885aea509bddce7ef
Lucas Gardini <lucasgardini.dias@hotmail.com>
2021-05-09 22:40:58 -0300

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,62 @@
 <template>
-	<div class="container">
-		<div>
-			<Logo />
-			<h1 class="title">MegaSenhaApp</h1>
-			<div class="links">
-				<a
-					href="https://nuxtjs.org/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="button--green"
-				>
-					Documentation
-				</a>
-				<a
-					href="https://github.com/nuxt/nuxt.js"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="button--grey"
-				>
-					GitHub
-				</a>
-			</div>
-		</div>
-	</div>
+	<a-form :form="form">
+		<a-form-item
+			:label-col="formItemLayout.labelCol"
+			:wrapper-col="formItemLayout.wrapperCol"
+			label="Name"
+		>
+			<a-input
+				v-decorator="[
+					'username',
+					{
+						rules: [
+							{
+								required: true,
+								message: 'Insira seu nome de usuário',
+							},
+						],
+					},
+				]"
+				placeholder="Insira seu nome de usuário"
+			/>
+		</a-form-item>
+		<a-form-item
+			:label-col="formTailLayout.labelCol"
+			:wrapper-col="formTailLayout.wrapperCol"
+		>
+			<a-button type="primary" @click="googleLogin"> Entrar </a-button>
+		</a-form-item>
+	</a-form>
 </template>
 
 <script>
-export default {};
+const formItemLayout = {
+	labelCol: { span: 4 },
+	wrapperCol: { span: 8 },
+};
+const formTailLayout = {
+	labelCol: { span: 4 },
+	wrapperCol: { span: 8, offset: 4 },
+};
+export default {
+	data() {
+		return {
+			checkNick: false,
+			formItemLayout,
+			formTailLayout,
+			form: this.$form.createForm(this, { name: "dynamic_rule" }),
+		};
+	},
+	methods: {
+		async googleLogin() {},
+		handleChange(e) {
+			this.checkNick = e.target.checked;
+			this.$nextTick(() => {
+				this.form.validateFields(["nickname"], { force: true });
+			});
+		},
+	},
+};
 </script>
 
 <style>
